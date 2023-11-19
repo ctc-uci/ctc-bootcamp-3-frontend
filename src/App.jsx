1e6ad220f9bc01c8ee548b77a755263aa761486b
Ethan Ho <hoea2@uci.edu>
2023-11-18 18:46:02 -0800

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import HangmanView from './views/HangmanView/HangmanView';
 import SingleMember from './views/SingleMemberInfo/SingleMember';
 
+import NavBar from './components/NavBar/NavBar.jsx';
+
 const App = () => {
   return (
     <BrowserRouter>
+      <NavBar/>
       <Routes>
         <Route exact path="/hangman" element={<HangmanView/>}/>
         <Route path="/members/:memberId" element={<SingleMember />} />
