dd0c98f3c8407572fdebfa2babe664d5995cd695
subinqkim <subinlk@uci.edu>
2023-11-18 18:32:51 -0800

    Implemented individual member page that displays more info

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import HangmanView from './views/HangmanView/HangmanView';
+import SingleMember from './views/SingleMemberInfo/SingleMember';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/hangman" element={<HangmanView/>}/>
+        <Route path="/members/:memberId" element={<SingleMember />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/views/SingleMemberInfo/SingleMember.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom'; // Import useParams
+
+function SingleMember() {
+  // Use useParams to get the memberId from the route parameters
+  const { memberId } = useParams();
+  const [singleMember, setSingleMember] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log('memberId', memberId);
+        const response = await axios.get(`http://localhost:3001/members/${memberId}`);
+        console.log('response', response.data);
+        const firstMem = response.data[0];
+        setSingleMember(firstMem);
+      } catch (error) {
+        console.error('Error fetching member information:', error.message);
+      }
+    };
+
+    fetchData();
+  }, [memberId]);
+
+  return (
+    <div>
+      <h1>Member Information</h1>
+      {singleMember ? (
+        <div>
+          <p>ID: {singleMember.id}</p>
+          <p>Name: {singleMember.memberName}</p>
+          <p>Year: {singleMember.memberYear}</p>
+          <p>Major: {singleMember.majorsMinors}</p>
+          <p>Project: {singleMember.project}</p>
+          <p>MBTI: {singleMember.mbti}</p>
+          <p>Birthday: {singleMember.birthday}</p>
+          <p>Spirit Animal: {singleMember.spiritAnimal}</p>
+          <p>Bio: {singleMember.bio}</p>
+          <p>Funny Quote: {singleMember.funnyQuote}</p>
+          <p>Image Link: {singleMember.imageLink}</p>
+          {/* Renders*/}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default SingleMember;
